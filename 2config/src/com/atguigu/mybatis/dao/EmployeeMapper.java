b85671d7f69115e4e0ddd5688afabf1608af784b
Hushunkang <2752962035@qq.com>
2020-03-29 11:13:18 +0800

    mybatis mybatis移植性的体现（根据不同的数据库厂商执行不同sql）databaseIdProvider，即支持多数据库厂商，支持多数据库

---
+++ 2config/src/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.bean.Employee;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月29日 07时13分25秒
+ */
+public interface EmployeeMapper {
+
+    Employee getEmpById(Integer id);
+
+}
