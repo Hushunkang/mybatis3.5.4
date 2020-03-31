032854e4385ba41cf9f4926869889ecdf0696b0d
Hushunkang <2752962035@qq.com>
2020-03-31 19:00:46 +0800

    mybatis 缓存有关的设置及属性

--- 5cache/src/com/atguigu/mybatis/test/MyBatisTest.java
+++ 5cache/src/com/atguigu/mybatis/test/MyBatisTest.java
@@ -64,27 +64,27 @@ public class MyBatisTest {
      * 	                EmployeeMapper===>Employee
      * 	                DepartmentMapper===>Department
      * 			        不同namespace查出的数据会放在自己对应的二级缓存中（缓存也是一个对象，mybatis中二级缓存的本质就是一个map，很多缓存的设计思想类似，差不多都是map）
+     *
      * 	使用：
      * 	1）、在mybatis的全局配置文件中配置<setting name="cacheEnabled" value="true"/>
      * 	2）、去***Mapper.xml中配置使用二级缓存<cache></cache>，在哪个mybatis的sql映射文件中配置了这个cache标签，哪个namespace就有二级缓存的功能，否则没有二级缓存的功能
      * 	3）、我们的POJO类需要实现序列化接口
+     *
      * 	效果：
      * 	数据会从二级缓存中获取，查出的数据都会被默认先放在一级缓存中，只有sql会话提交或者关闭以后，一级缓存中的数据才会转移到二级缓存中
      *
      * 和缓存有关的设置/属性：
-     * 			1）、cacheEnabled=true：false：关闭缓存（二级缓存关闭）(一级缓存一直可用的)
-     * 			2）、每个select标签都有useCache="true"：
-     * 					false：不使用缓存（一级缓存依然使用，二级缓存不使用）
-     * 			3）、【每个增删改标签的：flushCache="true"：（一级二级都会清除）】
-     * 					增删改执行完成后就会清楚缓存；
-     * 					测试：flushCache="true"：一级缓存就清空了；二级也会被清除；
-     * 					查询标签：flushCache="false"：
-     * 						如果flushCache=true;每次查询之后都会清空缓存；缓存是没有被使用的；
-     * 			4）、sqlSession.clearCache();只是清楚当前session的一级缓存；
-     * 			5）、localCacheScope：本地缓存作用域：（一级缓存SESSION）；当前会话的所有数据保存在会话缓存中；
-     * 								STATEMENT：可以禁用一级缓存；
+     *  1）、mybatis的全局配置文件中cacheEnabled=true/false：是否开启还是关闭二级缓存（一级缓存一直可用的）
+     * 	2）、每个select标签都有useCache=true/false，设置成false表示不使用二级缓存（不使用二级缓存，依然使用一级缓存）
+     * 	3）、每个增删改标签都有flushCache=true，默认为true表示增删改执行完成后就会清除mybatis一级缓存和mybatis二级缓存中的数据
+     * 		每个查询标签都有flushCache=false，默认为false表示每次查询之后mybatis一级缓存和mybatis二级缓存都不会被清除
+     * 	    如果在查询标签中设置flushCache=true表示每次查询之后都会清除mybatis一级缓存和mybatis二级缓存中的数据，即mybatis的缓存是没有被使用的
+     * 	4）、sqlSession.clearCache()只是清除当前sql会话的一级缓存
+     * 	5）、localCacheScope本地缓存作用域，即表示mybatis一级缓存
+     * 	                    设置成SESSION表示当前sql会话的所有数据保存在sql会话缓存中
+     * 					    设置成STATEMENT表示可以禁用一级缓存
      *
-     *第三方缓存整合：
+     * 第三方缓存整合：
      *		1）、导入第三方缓存包即可；
      *		2）、导入与第三方缓存整合的适配包；官方有；
      *		3）、mapper.xml中使用自定义缓存
@@ -194,7 +194,7 @@ public class MyBatisTest {
             System.out.println(employee2);
             System.out.println(employee1 == employee2);//若二级缓存的readOnly属性设置为true这个地方返回true，否则返回false
 
-            EmployeeMapper employeeMapper3 = sqlSession2.getMapper(EmployeeMapper.class);
+            EmployeeMapper employeeMapper3 = sqlSession3.getMapper(EmployeeMapper.class);
             //第三次查询是从二级缓存中获取的数据，并没有重新发送sql Cache Hit Ratio 缓存命中率 2/3
             Employee employee3 = employeeMapper3.getEmpById(1);
             System.out.println(employee3);
