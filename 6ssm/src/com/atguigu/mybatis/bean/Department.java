73f3b0e831ab3de9d9532cbd78a7a6c4c7c284cc
Hushunkang <2752962035@qq.com>
2020-03-31 21:51:01 +0800

    mybatis ssm整合 编写mybatis配置文件

---
+++ 6ssm/src/com/atguigu/mybatis/bean/Department.java
@@ -0,0 +1,64 @@
+package com.atguigu.mybatis.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月30日 15时12分20秒
+ */
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 4056536981905764933L;
+
+    private Integer id;
+    private String departmentName;
+    private List<Employee> emps;
+
+    public Department(Integer id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(List<Employee> emps) {
+        this.emps = emps;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Department() {
+    }
+
+    public Department(Integer id, String departmentName) {
+        this.id = id;
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+
+}
---
+++ 6ssm/src/com/atguigu/mybatis/bean/Employee.java
@@ -0,0 +1,89 @@
+package com.atguigu.mybatis.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月28日 18时06分17秒
+ */
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = -1586826091673542093L;
+
+    private Integer id;
+    private String lastName;
+    private Character gender;
+    private String email;
+    private Department dept;
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String lastName, Character gender, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public Employee(Integer id, String lastName, Character gender, String email, Department dept) {
+        this.id = id;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        this.dept = dept;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
