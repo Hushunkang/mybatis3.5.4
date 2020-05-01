c29d5928844eacafb9ced4f9d02aed076e11486d
Hushunkang <2752962035@qq.com>
2020-05-01 18:27:19 +0800

    建议在生产环境上面禁用掉mybatis的缓存机制，单纯的当一个ORM框架使用即可

--- 5cache/src/com/atguigu/mybatis/test/MyBatisTest.java
+++ 5cache/src/com/atguigu/mybatis/test/MyBatisTest.java
@@ -51,11 +51,12 @@ public class MyBatisTest {
      * 以后如果需要获取相同的数据，直接从缓存中拿，没必要再去查询数据库
      *
      * 一级缓存失效的情况（没有使用到当前一级缓存的情况，效果就是，还需要再向数据库发出查询sql）
-     * 1、两次获取相同的数据，但是两次使用的SqlSession对象（底层还是JDBC的java.sql.Connection对象）不同，说白了两次获取相同的数据但是它们使用了不同的数据库连接对象
+     * 1、两次获取相同的数据，但是两次使用的SqlSession对象（底层还是JDBC的java.sql.Connection对象）不同，说白了两次获取相同的数据（两次JDBC操作用的是不同的数据库连接对象）但是它们使用了不同的数据库连接对象
      * 2、两次使用的SqlSession对象相同，但是两次查询条件不同（原理是因为当前一级缓存里面还没有这个数据）
      * 3、两次使用的SqlSession对象相同，但是两次查询之间执行了增删改操作（原理是因为执行的增删改操作可能对当前数据有影响，相当于一级缓存中的数据有可能是过时的)
      * 4、两次使用的SqlSession对象相同，但是手动清除一级缓存（清空缓存）
      *
+     * 如果多个SqlSession之间需要共享缓存，则需要使用到二级缓存，二级缓存实现了多个SqlSession之间共享缓存
      * mybatis二级缓存：全局缓存，namespace级别的缓存，一个namespace对应着一个mybatis的sql映射文件，一个namespace对应着一个二级缓存
      * 工作机制：
      *  1、一个sql会话，查询一条数据，这个数据就会被放在当前sql会话的一级缓存中
@@ -71,9 +72,21 @@ public class MyBatisTest {
      * 	3）、我们的POJO类需要实现序列化接口
      *
      * 	开启mybatis二级缓存的最终效果：
-     * 	mybatis在查询数据的时候，会先从二级缓存中获取数据
-     * 	查出的数据都会被默认先放在一级缓存中
-     * 	只有sql会话提交或者关闭以后，一级缓存中的数据才会转移到二级缓存中
+     * 	mybatis在查询数据的时候，会先从二级缓存中查询数据（map.get(namespace+id+paramValue)）
+     * 	若从二级缓存中查出数据，则数据都会被默认先放在一级缓存中，完成查询任务
+     * 	若从二级缓存中查不出数据，则去一级缓存中查询数据，查出数据，完成查询任务
+     * 	若从一级缓存中还查不出数据，则发送sql查询数据库，查询出的数据放到一级缓存里面，完成查询任务
+     * 	注意：只有sql会话提交或者关闭以后，一级缓存中的数据才会被转移到二级缓存中
+     *
+     * 	补充：mybatis一级缓存的底层数据结构是键值对的，它的key是StatementId + Offset + Limit + Sql + Params，它的value是查询出的数据
+     * 	     只要两条SQL的这五个值拼接一起是相同的，即可以认为是相同的查询操作
+     * 	     泛泛的讲，在一级缓存没有失效的前提下，第一次查询数据发送sql查询数据库，后面相同的查询操作不用发送sql查询数据库
+     *       myBatis一级缓存的生命周期和SqlSession一致
+     *       myBatis一级缓存内部设计简单，只是一个没有容量限定的HashMap，在缓存的功能性上有所欠缺
+     *       myBatis一级缓存最大范围是SqlSession内部，有多个SqlSession或者微服务架构下，数据库写操作会引起读到脏数据的问题，建议设定mybatis一级缓存的级别为Statement
+     *       即在mybatis全局配置文件中添加如下设置<setting name="localCacheScope" value="STATEMENT"/>，而不是默认的SESSION级别<setting name="localCacheScope" value="SESSION"/>
+     *       其实建议在生产环境上面禁用掉mybatis的缓存机制，单纯的当一个ORM框架使用即可
+     * 	     参考资料：https://tech.meituan.com/2018/01/19/mybatis-cache.html
      *
      * 和缓存有关的设置/属性：
      *  1）、mybatis的全局配置文件中cacheEnabled=true/false：是否开启还是关闭二级缓存（一级缓存一直可用的）
@@ -86,7 +99,7 @@ public class MyBatisTest {
      * 	                    设置成SESSION表示当前sql会话的所有数据保存在sql会话缓存中
      * 					    设置成STATEMENT表示可以禁用一级缓存
      *
-     * mybatis与第三方缓存整合：
+     * mybatis与第三方缓存整合（mybatis自己预留了Cache接口，给自己留条后路，毕竟自己不是专业做缓存的，预留的Cache接口就是为了整合第三方缓存）：
      *	1）、导入第三方缓存包ehcache-core-2.6.8.jar
      *	2）、导入与第三方缓存整合的适配包，适配包在mybatis在github上面地址里面有下载mybatis-ehcache-1.0.3.jar
      *	3）、***Mapper.xml中使用自定义缓存
@@ -183,10 +196,10 @@ public class MyBatisTest {
         SqlSession sqlSession3 = sqlSessionFactory.openSession();
         try {
             EmployeeMapper employeeMapper1 = sqlSession1.getMapper(EmployeeMapper.class);
-            //第一次查询 Cache Hit Ratio 缓存命中率 0
-            Employee employee1 = employeeMapper1.getEmpById(1);
+            //第一次查询是从数据库中获取的数据，发送sql Cache Hit Ratio 缓存命中率 0
+            Employee employee1 = employeeMapper1.getEmpById(1);//发送sql查询数据库，查询出的数据放到一级缓存里面，完成查询任务
             System.out.println(employee1);
-            sqlSession1.close();
+            sqlSession1.close();//注意：只有sql会话提交或者关闭以后，一级缓存中的数据才会被转移到二级缓存中
 
             EmployeeMapper employeeMapper2 = sqlSession2.getMapper(EmployeeMapper.class);
             //第二次查询是从二级缓存中获取的数据，并没有重新发送sql Cache Hit Ratio 缓存命中率 1/2
